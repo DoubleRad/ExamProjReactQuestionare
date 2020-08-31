f248764c42288272f0f3db40c2592403f8d99262
Gordienko Rodion <rusyaloveee@gmail.com>
2020-08-31 18:11:31 +0300

    Добавил возможность выгрузить ответы json'ом

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require( 'http' ) ;
 const fs   = require('fs') ;
+const formidable = require( 'formidable' ) ;
  
 function flushFile( response, fileName ) {
     fs.readFile( fileName, (err, data) => {
@@ -15,6 +16,37 @@ function serverFunction( request, response ) {
 
     var url = request.url.substr( 1 ) ;  // Убираем первый символ (/) из запроса
     var method = request.method.toUpperCase() ;
+
+    if (method == 'POST' ) {
+        if( url == 'upload' ) {
+            console.log('upload прошла')
+            var form = formidable.IncomingForm( )
+
+            form.parse( request , function( err, fields, files ) {
+                if( err ){
+                    console.error( err ) ;
+                    return;
+                }
+
+                console.log( fields ) ;
+
+                if( fields ) {
+                    fs.readFile( 'answers.json' , (err, data)=>{ // Читаем Answer.json
+                        var fieldsList = JSON.parse( data ) ;   // Парсим Answer в объект
+                        for(key in fields){ // На каждой итерации пушим в Answer вопрос и ответ на него
+                            var a = {  };
+                            a[key] = fields[key];
+                            fieldsList.push( a ) ;
+                            console.log( key +" : " + fields[key] )
+                        }
+                        fs.writeFile( 'answers.json', JSON.stringify( fieldsList ), ()=>{} ) ; // Сохраняю Answer.json
+                    } ) ;
+                    flushFile( response, 'home.html' ) ;
+                }
+            } ) ; 
+            return ;
+        }
+    }
 
     if( url == "" ) {  // Пустой запрос (localhost/) - домашняя страница
         url = 'home.html' ;
@@ -43,7 +75,7 @@ function serverFunction( request, response ) {
 
     
  
-    response.end( "<h1>Gallery</h1>" ) ;
+    response.end( "<h1 style='text-align:center'>404</h1>" ) ;
 }
  
 http.createServer( serverFunction ).listen( 88, () => {
--- my.js
+++ my.js
@@ -37,34 +37,36 @@ class Questionnaire extends React.Component{
 
     render( ){
         return <div className='questionnaire_container_obj'>
-            <table>
-                <tbody>
-                    <tr>
-                        <th className='section'>Вопросы</th>
-                        <th className='section'>Варианты ответов</th>
-                    </tr>
-
-                    { this.state.questions.map( q => <tr>
-
-                        <input type='radio' value={ 'Bad' }    name={q.id + 'qst'}  id={q.id + 'qst' + 'BadId'}    hidden ></input>
-                        <input type='radio' value={ 'Normal' } name={q.id + 'qst'}  id={q.id + 'qst' + 'NormalId'} hidden ></input>
-                        <input type='radio' value={ 'Good' }   name={q.id + 'qst'}  id={q.id + 'qst' + 'GoodId'}   hidden ></input>
-
-                           
-                            <td className='question_text'>{q.txt}</td>
-                            <td className='label_container'>    
-                                <label for={q.id + 'qst' + 'BadId'}     className="radio_label label_1 badAnsw" onClick={this.mark}  >   </label>
-                                <label for={q.id + 'qst' + 'NormalId'}  className="radio_label label_2 normAnsw" onClick={this.mark} >   </label>
-                                <label for={q.id + 'qst' + 'GoodId'}    className="radio_label label_3 goodAnsw" onClick={this.mark} >   </label>
-                            </td>
+            <form method="POST" action="upload" enctype="multipart/form-data">    
+                <table>
+                    <tbody>
+                        <tr>
+                            <th className='section'>Вопросы</th>
+                            <th className='section'>Варианты ответов</th>
                         </tr>
-                    )}
-                        
 
-                        <input type='submit' id='submitButton' hidden></input>
-                        <label className='sbmtBtn' for='submitButton'></label>
-                </tbody>
-            </table>
+                        { this.state.questions.map( q => <tr>
+
+                                <input type='radio' value={ 'Bad' }    name={q.id + 'qst'}  id={q.id + 'qst' + 'BadId'}    hidden ></input>
+                                <input type='radio' value={ 'Normal' } name={q.id + 'qst'}  id={q.id + 'qst' + 'NormalId'} hidden ></input>
+                                <input type='radio' value={ 'Good' }   name={q.id + 'qst'}  id={q.id + 'qst' + 'GoodId'}   hidden ></input>
+
+                            
+                                <td className='question_text'>{q.txt}</td>
+                                <td className='label_container'>    
+                                    <label for={q.id + 'qst' + 'BadId'}     className="radio_label label_1 badAnsw" onClick={this.mark}  >   </label>
+                                    <label for={q.id + 'qst' + 'NormalId'}  className="radio_label label_2 normAnsw" onClick={this.mark} >   </label>
+                                    <label for={q.id + 'qst' + 'GoodId'}    className="radio_label label_3 goodAnsw" onClick={this.mark} >   </label>
+                                </td>
+                            </tr>
+                        ) }
+                            
+
+                            <input type='submit' id='submitButton' hidden></input>
+                            <label className='sbmtBtn' for='submitButton'></label>
+                    </tbody>
+                </table>
+            </form>
         </div>
     }
 
